001378002b79bbde4152c452265e69db0fca09db
YoussefTurkey <youssef.turkey11@gmail.com>
2023-03-08 14:12:29 +0200

    covert data to JSON file & fix some responsive style

--- src/view/Header.jsx
+++ src/view/Header.jsx
@@ -1,18 +1,9 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import personalImg from "../assets/images/me2.jpg"
+import personal from "../assets/js/information.json"
 
 const Header = () => {
-
-    const HeaderInfo = {
-        "fullName" : "Youssef Turkey",
-        "title" : "Junior Frontend Developer",
-        "phone" : "+201273451052",
-        "country" : "Alexandria, Egypt",
-        "gmail" : "youssef.turkey11@gmail.com",
-        "socialContact" : "linkedin.com/in/youturkey11"
-    }
-
   return (
     <>
         <header className="header">
@@ -21,30 +12,30 @@ const Header = () => {
                     <img loading='lazy' src={personalImg} alt="personal-image" />
                     
                     <div className="fullName">
-                        <h1>{HeaderInfo.fullName}</h1>
-                        <h3>{HeaderInfo.title}</h3>
+                        <h1>{personal[0].Turkey.HeaderInfo.fullName}</h1>
+                        <h3>{personal[0].Turkey.HeaderInfo.title}</h3>
                     </div>
                 </Col>
                 
                 <Col className='col-12 col-sm-12 col-md-4 contact'>
                     <p>
                         <i className="fa-solid fa-phone"></i>
-                        <a href={`https://wa.me/${HeaderInfo.phone}`} target='_blank'>{HeaderInfo.phone}</a>
+                        <a href={`https://wa.me/${personal[0].Turkey.HeaderInfo.phone}`} target='_blank'>{personal[0].Turkey.HeaderInfo.phone}</a>
                     </p>
 
                     <p>
                         <i className="fa-solid fa-house"></i>
-                        <span>{HeaderInfo.country}</span>
+                        <span>{personal[0].Turkey.HeaderInfo.country}</span>
                     </p>
 
                     <p>
                         <i className="fa-sharp fa-solid fa-envelope"></i>
-                        <a href={`mailto:${HeaderInfo.gmail}`}>{HeaderInfo.gmail}</a>
+                        <a href={`mailto:${personal[0].Turkey.HeaderInfo.gmail}`}>{personal[0].Turkey.HeaderInfo.gmail}</a>
                     </p>
 
                     <p>
                         <i className="fa-brands fa-linkedin"></i>
-                        <a href={`https://www.${HeaderInfo.socialContact}`} target='_blank'>{HeaderInfo.socialContact}</a>
+                        <a href={`https://www.${personal[0].Turkey.HeaderInfo.socialContact}`} target='_blank'>{personal[0].Turkey.HeaderInfo.socialContact}</a>
                     </p>
                 </Col>
             </Row>
--- src/view/Sidebar.jsx
+++ src/view/Sidebar.jsx
@@ -1,28 +1,12 @@
-import React from 'react'
+import personal from "../assets/js/information.json"
 
 const Sidebar = () => {
-
-  const sideInfo = {
-    "education" : "Bachelor of Science, Alexandria University | 2022",
-    "techSkills" : [
-      ["HTML5, ", "CSS3, ", "JavaScript, ", "Scss"],
-      ["Bootstrap, ", "MDB, ", "jQuery"],
-      ["React.js, ", "vue.js"],
-      ["Photoshop, ", "illustrator, ", "lightroom"]
-    ],
-    "certificates" : ["Diploma of Quantum Computing & Programming (2022)", 
-                      "AWCO Training center (2021)", 
-                      "Complete HTML5 & CSS3 & JavaScript Course (2020)"
-                    ],
-    "languages" : ["Arabic - Native", "English - Elementary"]
-  }
-
   return (
     <>
       <section className='sidebar'>
         <div className="cel">
           <h2>Education</h2>
-          <p>{sideInfo.education}</p>
+          <p>{personal[0].Turkey.sideInfo.education}</p>
         </div>
 
         <div className="cel">
@@ -30,35 +14,35 @@ const Sidebar = () => {
 
           <ul>
             <h3>Technical Skills</h3>
-            <li>{sideInfo.techSkills[0]}.</li>
-            <li>{sideInfo.techSkills[1]}.</li>
-            <li>{sideInfo.techSkills[2]}.</li>
-            <li>{sideInfo.techSkills[3]}.</li>
+            <li>{personal[0].Turkey.sideInfo.techSkills[0]}, {personal[0].Turkey.sideInfo.techSkills[1]}, {personal[0].Turkey.sideInfo.techSkills[2]}, {personal[0].Turkey.sideInfo.techSkills[3]}.</li>
+            <li>{personal[0].Turkey.sideInfo.techSkills[4]}, {personal[0].Turkey.sideInfo.techSkills[5]}, {personal[0].Turkey.sideInfo.techSkills[6]}.</li>
+            <li>{personal[0].Turkey.sideInfo.techSkills[7]}, {personal[0].Turkey.sideInfo.techSkills[8]}.</li>
+            <li>{personal[0].Turkey.sideInfo.techSkills[9]}, {personal[0].Turkey.sideInfo.techSkills[10]}, {personal[0].Turkey.sideInfo.techSkills[11]}.</li>
           </ul>
 
           <ul>
             <h3>Soft Skills</h3>
-            <li>Teamwork, leadership.</li>
-            <li>Communication, presentation.</li>
-            <li>Self-learning.</li>
-            <li>Critical thinking, creative.</li>
+            <li>{personal[0].Turkey.sideInfo.softSkills[0]}, {personal[0].Turkey.sideInfo.softSkills[1]}.</li>
+            <li>{personal[0].Turkey.sideInfo.softSkills[2]}, {personal[0].Turkey.sideInfo.softSkills[3]}.</li>
+            <li>{personal[0].Turkey.sideInfo.softSkills[4]}.</li>
+            <li>{personal[0].Turkey.sideInfo.softSkills[5]}, {personal[0].Turkey.sideInfo.softSkills[6]}.</li>
           </ul>
         </div>
 
         <div className="cel cert">
           <h2>Certificates</h2>
           <ul className='certificates'>
-            <li>{sideInfo.certificates[0]}</li>
-            <li>{sideInfo.certificates[1]}</li>
-            <li>{sideInfo.certificates[2]}</li>
+            <li>{personal[0].Turkey.sideInfo.certificates[0]}</li>
+            <li>{personal[0].Turkey.sideInfo.certificates[1]}</li>
+            <li>{personal[0].Turkey.sideInfo.certificates[2]}</li>
           </ul>
         </div>
 
         <div className="cel">
           <h2>languages</h2>
           <ul className='languages'>
-            <li>{sideInfo.languages[0]}</li>
-            <li>{sideInfo.languages[1]}</li>
+            <li>{personal[0].Turkey.sideInfo.languages[0]}</li>
+            <li>{personal[0].Turkey.sideInfo.languages[1]}</li>
           </ul>
         </div>
       </section>
